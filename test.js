46ee1418911ef3a5ed6182902a518374e0909a9f
Gleb Khudyakov <g.khudyakov@hh.ru>
2016-06-30 13:35:51 +0300

    add test with mixed sync/async tasks

--- test.js
+++ test.js
@@ -1,46 +1,77 @@
 "use strict";
 const waterfall = require('./index');
 
-var tasks = [
-	function(cb) {
-		console.log(1);
-		cb(null);
-	},
-	function(cb){
-		console.log(2);
-		cb(null);
-	},
-	function(cb){
-		console.log(3);
-		cb(null);
-	},
-	function(cb){
-		console.log(4);
-		cb(null);
-	},
-	function(cb){
-		console.log(5);
-		cb(null);
+var generateSyncTask = function(index) {
+	return function (x){
+		return function(cb){
+			console.log(x);
+			cb(null);
+		};
+	}(index);
+};
+
+
+var generateAsyncTask = function(index) {
+	return function (x){
+		return function(cb){
+			setTimeout(function(){
+				console.log(x);
+				cb(null);
+			}, 0);
+		};
+	}(index);
+};
+
+var generateSyncTasks = function(count){
+	var tasks = [];
+	for(var i=0; i<count; i++) {
+		tasks.push(generateSyncTask(i));
 	}
-];
+	return tasks;
+}
+
+var generateAsyncTasks = function(count){
+	var tasks = [];
+	for(var i=0; i<count; i++) {
+		tasks.push(generateAsyncTask(i));
+	}
+	return tasks;
+}
+
+
+var generateRandomTasks = function(count){
+	var tasks = [];
+	for(var i=0; i<count; i++) {
+		Math.random() > .5 ? tasks.push(generateAsyncTask(i)) : tasks.push(generateSyncTask(i))
+	}
+	return tasks;
+}
 
 var done = function(){
 	console.log('done');
 }
 
 var testSync = function(){
-	waterfall(tasks, done);
+	waterfall(generateSyncTasks(10), done);
 	console.log('this text should be after waterfall');
 
 };
 
 var testAsync = function(){
-	waterfall(tasks, done, true);
+	waterfall(generateAsyncTasks(5), done);
 	console.log('this text should be before waterfall');
 };
+
+var testMixed = function(){
+	waterfall(generateRandomTasks(20), done);
+};
+
 
 console.log('testSync:');
 testSync();
 
-console.log('\ntestAsync: ');
-testAsync();
+// console.log('\ntestAsync: ');
+// testAsync();
+
+console.log('\ntestMixed: ');
+testMixed();
